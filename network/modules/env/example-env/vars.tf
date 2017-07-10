71926c260dd4a3af83efb2f3ba5734ce32207783
Stephen Augustus <augustus@coreos.com>
2017-07-10 04:04:20 -0700

    network: Remove commented out defaults

--- network/modules/env/example-env/vars.tf
+++ network/modules/env/example-env/vars.tf
@@ -1,12 +1,9 @@
 variable "rg_name" {
   description = "Specifies the Azure Resource Group to place resource(s) into."
-  default     = ""
 }
 
 variable "nsg_name" {
   description = "Specifies the network security group to associate rule to."
-
-  #default = ""
 }
 
 variable "env_subnet" {
@@ -14,6 +11,4 @@ variable "env_subnet" {
 Denotes the subnet CIDR of the current environment.
 Expected format: "a.b.c.d/xx"
 EOF
-
-  #default = ""
 }
